2dbd0faa268c3cfa10b5a83247519120825688f4
Cameron Lee <cameronslee@hotmail.com>
2023-07-11 10:10:49 -0700

    custom app colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,19 @@
 import { type Config } from "tailwindcss";
+const colors = require('tailwindcss/colors')
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'threads-bg': {
+        light: 'rgb(255, 255, 255)',
+        dark: 'rgb(16,16,16)',
+      },
+      'threads-text': {
+        light: 'rgb(255,255,255)',
+        dark: 'rgb(16, 16, 16)',
+      },
+    },
     extend: {},
   },
   plugins: [],
